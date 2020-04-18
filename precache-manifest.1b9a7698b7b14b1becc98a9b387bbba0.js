4c00e39b633c88aac8862e593e4b61966961a3e9
farhajhussain44 <farhajhussain29@gmail.com>
2020-04-19 00:38:53 +0530

    Updates

---
+++ precache-manifest.1b9a7698b7b14b1becc98a9b387bbba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d29057ca2c74504e9b8d53302bb4cd4",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "fd5d650f72d85dfc5300",
+    "url": "/coronatracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "c46cfecde8ff3d7fdd9f",
+    "url": "/coronatracker/static/js/2.027b86c6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.027b86c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5d650f72d85dfc5300",
+    "url": "/coronatracker/static/js/main.d4afa176.chunk.js"
+  },
+  {
+    "revision": "a7d13efdf1d2016d58de",
+    "url": "/coronatracker/static/js/runtime-main.7078cebf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
